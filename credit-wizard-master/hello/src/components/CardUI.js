033f220d362c9494a57d63280d23df7c419806e7
mehakgupta200 <67285974+mehakgupta200@users.noreply.github.com>
2021-06-02 23:01:00 +0530

    mehak change 1

---
+++ credit-wizard-master/hello/src/components/CardUI.js
@@ -0,0 +1,95 @@
+import React, { useState, useEffect } from "react";
+import { sendAxiosHit } from "../utils";
+import { useAuth } from "../auth";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./card-style.css";
+import Form from "react-bootstrap/Form";
+
+
+export default function Card(props) {
+  const { item, auth, getCustomers } = props;
+  const [bal, setBal] = useState();
+  const [paidAmount, setPaidAmount] = useState(item.amount_paid);
+  console.log("item and aurt and getCustomers", item, auth, getCustomers);
+
+  async function updateCustomer(value) {
+    const UPDATE_CUSTOMER = `mutation{
+        update_customers_by_pk(
+          pk_columns:{id:${item.id}}
+          _set:{amount_paid:${value},
+        balance:${item.credit - value}}
+        ){
+          id
+          name
+           email
+                credit
+                amount_paid
+                is_completed
+                merchant_id
+                balance
+        }
+      }`;
+
+    try {
+      const response = await sendAxiosHit(UPDATE_CUSTOMER, auth.idToken);
+      console.log("user deets", response);
+      if (response.errors && response.errors.length !== 0) {
+        console.log("fb expire error");
+      } else if (response?.data?.update_customers_by_pk) {
+        console.log("in update if ", response);
+        // calculateBalance(value);
+          window.location.reload(false);
+           }
+      return response;
+    } catch (error) {
+      console.log("Error?????", error);
+      return { error: error };
+    }
+  }
+
+  return (
+    <div className="card">
+      <div className="card-body text-dark">
+        <Form>
+          <Form.Group>
+            <Form.Label>name</Form.Label>
+            <Form.Control type="text" value={item.name} readOnly />
+
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" value={item.email} readOnly />
+
+            <Form.Label>Credit</Form.Label>
+            <Form.Control
+              type="text"
+              //   placeholder="Enter credit amount"
+              value={item.credit}
+              readOnly
+            />
+
+            <Form.Label>Paid amount</Form.Label>
+            <Form.Control
+              type="text"
+              defaultValue={item.amount_paid}
+              onChange={(e) => {
+                setPaidAmount(e.target.value);
+              }}
+            />
+            <Form.Label>Balance</Form.Label>
+            <Form.Control type="text" value={item.balance} readOnly />
+          </Form.Group>
+        </Form>
+        <br></br>
+        <button
+          type="button"
+          class="btn btn-outline-success"
+          onClick={() => {
+            console.log("paidAmount", paidAmount);
+            updateCustomer(paidAmount);
+          }}
+        >
+          Add Transaction
+        </button>
+      </div>
+    </div>
+  );
+}
---
+++ credit-wizard-master/hello/src/components/Dashboard.js
@@ -0,0 +1,242 @@
+import React, { Component, useState, useEffect } from "react";
+import axios from "axios";
+import firebase from "../firebase";
+import { sendAxiosHit } from "../utils";
+import { useAuth } from "../auth";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Card from "./CardUI";
+import "./card-style.css";
+import Modal from "react-bootstrap/Modal";
+import Form from "react-bootstrap/Form";
+import Button from "react-bootstrap/Button";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+
+export default function Dashboard(props) {
+  const [change, setChange] = useState(false);
+  const auth = useAuth();
+  console.log("dashboard auth", auth);
+  const [show, setShow] = useState(false);
+  const [customerArray, setCustomerArray] = useState([]);
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [credit, setCredit] = useState();
+  const [paid, setPaid] = useState();
+
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  async function getCustomers() {
+    const GET_CUSTOMERS = `query{
+      customers{
+        id
+        name
+        email
+        credit
+        amount_paid
+        is_completed
+        merchant_id
+balance
+      }
+    }`;
+
+    try {
+      const response = await sendAxiosHit(GET_CUSTOMERS, auth.idToken);
+      console.log("user deets", response);
+      if (response.errors && response.errors.length !== 0) {
+        console.log("fb expire error");
+      } else if (
+        response?.data?.customers &&
+        response.data.customers.length !== 0
+      ) {
+        console.log("in if ", response);
+        setCustomerArray(response.data.customers);
+      }
+      return response;
+    } catch (error) {
+      console.log("Error?????", error);
+      return { error: error };
+    }
+  }
+
+  async function enterCustomer() {
+    handleClose();
+    console.log("mehekk1");
+    let balance = credit - paid;
+    const ADD_CUSTOMER = `mutation{
+    insert_customers_one(
+      object:{
+        name:"${name}",
+        email:"${email}",
+        credit:${credit},
+        amount_paid:${paid},
+        is_completed:false,
+        merchant_id:"${auth.idToken}",
+        balance:${balance}
+      }
+    ) {
+      id
+      name
+      email
+      credit
+      amount_paid
+      is_completed
+      merchant_id
+    }
+  }
+  `;
+
+    try {
+      console.log("mehekk2");
+      const response = await sendAxiosHit(ADD_CUSTOMER, auth.idToken);
+      console.log("user deets", response);
+      if (response.errors && response.errors.length !== 0) {
+        console.log("fb expire error");
+      } else if (
+        response?.data?.customers &&
+        response.data.customers.length !== 0
+      ) {
+        console.log("in if ", response);
+        // setCustomerArray(response.data.customers);
+      }
+      window.location.reload(false);
+      return response;
+    } catch (error) {
+      console.log("Error?????", error);
+      return { error: error };
+    }
+  }
+
+  useEffect(() => {
+    if (auth && auth.user) {
+      console.log("unique sa");
+      getCustomers();
+    }
+  }, [auth, change]);
+
+  console.log("customer arry", customerArray);
+
+  return (
+    <>
+      {auth && auth.idToken ? (
+        <>
+          {" "}
+          <div>
+            <Row>
+              <Col md={6}>
+                <div
+                  className="home-hero justify-content-center"
+                  style={{ padding: 40 }}
+                >
+                  <h1 className="text-center">Welcome Mehak</h1>
+                </div>
+              </Col>
+            </Row>
+          </div>
+          <div className="container-fluid d-flex justify-content">
+            <div className="row">
+              {customerArray.length !== 0 &&
+                customerArray.map((item) => (
+                  <div className="col">
+                    <Card item={item} auth={auth} getCustomers={getCustomers} />
+                  </div>
+                ))}
+            </div>
+            <div className="footer">
+              <button
+                type="button"
+                className="btn btn-primary"
+                onClick={handleShow}
+              >
+                ADD CUSTOMER
+              </button>
+            </div>
+            <Modal show={show} onHide={handleClose}>
+              <Modal.Header closeButton>
+                <Modal.Title>ADD CUSTOMER</Modal.Title>
+              </Modal.Header>
+              <Modal.Body>
+                <Form>
+                  <Form.Group>
+                    <Form.Label>name</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter name"
+                      onChange={(e) => {
+                        setName(e.target.value);
+                      }}
+                    />
+                  </Form.Group>
+
+                  <Form.Group controlId="formBasicEmail">
+                    <Form.Label>Email address</Form.Label>
+                    <Form.Control
+                      type="email"
+                      placeholder="Enter email"
+                      onChange={(e) => {
+                        setEmail(e.target.value);
+                      }}
+                    />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Credit</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="Enter credit amount"
+                      onChange={(e) => {
+                        setCredit(e.target.value);
+                      }}
+                    />
+                  </Form.Group>
+
+                  <Form.Group>
+                    <Form.Label>Paid amount</Form.Label>
+                    <Form.Control
+                      type="text"
+                      placeholder="0"
+                      onChange={(e) => {
+                        setPaid(e.target.value);
+                      }}
+                    />
+                  </Form.Group>
+                  <Form.Group>
+                    <Button
+                      // type="submit"
+                      onClick={() => {
+                        console.log("hewwo uwu");
+                        enterCustomer();
+                      }}
+                    >
+                      Save
+                    </Button>
+                  </Form.Group>
+                </Form>
+              </Modal.Body>
+            </Modal>
+          </div>
+        </>
+      ) : (
+        <div> Not Logged In</div>
+      )}
+    </>
+  );
+
+  // return (
+  //   <>
+  //     {auth && auth.user ? (
+  //       <div>
+  //         <button
+  //           onClick={() => {
+  //             sendAxiosHit("q", auth.idToken);
+  //           }}
+  //         >
+  //           hello {auth.idToken}
+  //         </button>
+  //       </div>
+  //     ) : (
+  //       <div> Not Logged In</div>
+  //     )}
+  //   </>
+  // );
+}
