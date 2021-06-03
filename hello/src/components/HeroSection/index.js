c3aa8207804d3b2a03501dd14fc3552ff1fed890
paruladitya <paruladitya3@gmail.com>
2021-06-03 13:24:13 +0530

    homepage text corrected

--- hello/src/components/HeroSection/index.js
+++ hello/src/components/HeroSection/index.js
@@ -1,34 +1,47 @@
-import React,{useState} from "react";
+import React, { useState } from "react";
 import Video from "../../videos/video.mp4";
-import {Button} from '../ButtonElement';
-import { HeroContainer, HeroBg, VideoBg,HeroContent,HeroP,HeroH1,HeroBtnWrapper,ArrowForward,ArrowRight } from "./HeroElements";
+import { Button } from "../ButtonElement";
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroP,
+  HeroH1,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight,
+} from "./HeroElements";
 
 const HeroSection = () => {
-    const [hover,setHover] =useState(false);
+  const [hover, setHover] = useState(false);
 
-    const onHover = () => {
-        setHover(!hover);
-    }
+  const onHover = () => {
+    setHover(!hover);
+  };
   return (
-    <HeroContainer id='home'>
+    <HeroContainer id="home">
       <HeroBg>
         <VideoBg autoPlay loop muted src={Video} type="video/mp4" />
       </HeroBg>
       <HeroContent>
-        <HeroH1>Bussiness Hua Easy!</HeroH1>
-        <HeroP>
-          DIGITAL LEDGER
-        </HeroP>
+        <HeroH1>Business Hua Easy!</HeroH1>
+        <HeroP>DIGITAL LEDGER</HeroP>
         <HeroBtnWrapper>
-            <Button to="signup" onMouseEnter={onHover} onMouseLeave={onHover}
-            primary="true" dark="true"
+          <Button
+            to="signup"
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+            primary="true"
+            dark="true"
             smooth={true}
             duration={500}
             spy={true}
             exact="true"
-            offset={-80}>
-                Get started {hover? <ArrowForward/>:<ArrowRight/>}
-            </Button>
+            offset={-80}
+          >
+            Get started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
         </HeroBtnWrapper>
       </HeroContent>
     </HeroContainer>
